203a20b5cab335f8957b03cf3647325607af2c5e
tlswltjq <41179427+tlswltjq@users.noreply.github.com>
2023-09-04 16:53:32 +0900

    Time: 43 ms (91.74%), Space: 21.8 MB (12.91%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,10 +1,10 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        strs = []
+        strs : Deque= collections.deque()
         for chr in s:
             if chr.isalnum():
                 strs.append(chr.lower())
         while len(strs)>1 :
-            if strs.pop(0)!=strs.pop():
+            if strs.popleft()!=strs.pop():
                 return False
         return True
